0b2b1678c9e74a435aac6863bc785b922ad1b78b
Abhijit Jadhav <abhijit.jadhav1998@gmail.com>
2022-05-09 20:21:15 +0530

    added java project

---
+++ ai-based-attendance-app/src/main/java/com/abhijit/aibasedattendanceapp/AiBasedAttendanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.abhijit.aibasedattendanceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBasedAttendanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiBasedAttendanceAppApplication.class, args);
+	}
+
+}
---
+++ ai-based-attendance-app/src/test/java/com/abhijit/aibasedattendanceapp/AiBasedAttendanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhijit.aibasedattendanceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiBasedAttendanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
